848cb98a431cfa262d1ce39c9c3b2afc80156f6f
KazMaune <kikainvr@gmail.com>
2023-06-08 18:03:23 -0500

    Finished our getdisplaynumber to add proper commas, decimals etc

--- script.js
+++ script.js
@@ -57,7 +57,29 @@ class Calculator {
     }
 
     getDisplayerNumber(number) {
-        return number;
+        const stringNumber = number.toString();
+
+        const integerDigits = parseFloat(stringNumber.split('.')[0]);
+
+        const decimalDigits = stringNumber.split('.')[1];
+        
+        let integerDisplay;
+
+        if (isNaN(integerDigits)) {
+          integerDisplay = '';
+        } 
+
+        else {
+          integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 });
+        }
+
+        if (decimalDigits != null) {
+          return `${integerDisplay}.${decimalDigits}`;
+        } 
+
+        else {
+          return integerDisplay;
+        }
     }
 
     updateDisplay() {
